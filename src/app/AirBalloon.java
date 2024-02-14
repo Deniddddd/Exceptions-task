74c76764b0b501dca00eab8acfeba4064b4c2458
Your Name <fdfdfss@fedsf.com>
2024-02-14 06:47:59 +0200

    ready commit

--- src/app/AirBalloon.java
+++ src/app/AirBalloon.java
@@ -1,32 +1,36 @@
 package app;
 
 import java.util.Scanner;
-
 public class AirBalloon {
-    public static void main(String[] args) {
-        Scanner balloon = new Scanner(System.in);
-        System.out.println("Введіть число від 120 до 355:");
-        int number;
-
-        while (true) {
-            while (!balloon.hasNextInt()) {
-                System.out.println("Будь ласка, введіть ціле число:");
-                balloon.next();
-            }
-            number = balloon.nextInt();
-
-            if (number >= 120 && number <= 355) {
-                break; // Виходимо з циклу, якщо число вірне
-            } else {
-                System.out.println("Число повинно бути в межах від 120 до 355. Спробуйте ще раз:");
-            }
+    public void Air(int height) throws AirBalloonException {
+        if (height < 120 || height > 355) {
+            throw new AirBalloonException("Неможливо зняти показники за межами висотного діапазону (від 120 до 355 метрів).");
+        } else {
+            System.out.println("Показники стану повітря зняті успішно на висоті " + height + " метрів.");
         }
-
-        // Виводимо введене число
-        System.out.println("Ви ввели число: " + number);
-
-        // Закриваємо Scanner
-        balloon.close();
-//        fdsfsdfsdf
     }
+
 }
+//    public static void main(String[] args) {
+//        Scanner balloon = new Scanner(System.in);
+//        System.out.println("Введіть число від 120 до 355:");
+//        int number;
+//
+//        while (true) {
+//            while (!balloon.hasNextInt()) {
+//                System.out.println("Будь ласка, введіть ціле число:");
+//                balloon.next();
+//            }
+//            number = balloon.nextInt();
+//
+//            if (number >= 120 && number <= 355) {
+//                break;
+//            } else {
+//                System.out.println("Число повинно бути в межах від 120 до 355. Спробуйте ще раз:");
+//            }
+//        }
+//
+//        System.out.println("Ви ввели число: " + number);
+//
+//        balloon.close();
+
